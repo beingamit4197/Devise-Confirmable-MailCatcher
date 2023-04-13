bf271d8db25c298cf15a9e834f4b7001fcad4862
beingamit4197 <beingamit4197@gmail.com>
2023-04-13 14:11:41 +0530

    added google omniauth to devise

--- db/migrate/20230412075912_devise_create_users.rb
+++ db/migrate/20230412075912_devise_create_users.rb
@@ -6,6 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :full_name,          null: false, default: ""
+      t.string :uid,                null: false, default: ""
+      t.string :avatar_url,         null: false, default: ""
+      t.string :provider
 
       ## Recoverable
       t.string   :reset_password_token
